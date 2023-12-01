de49048f7dd67c87a48289984a5cdcafa9557b0e
Antoine Roumilhac <antoine.roumilhac@outlook.fr>
2023-12-01 18:47:45 +0100

    Added ok and bad token respones for authentication
    Added basic chat messages handling

--- chat-client/src/main.rs
+++ chat-client/src/main.rs
@@ -1,24 +1,45 @@
 use std::net::TcpStream;
-use std::io::{Write, BufReader, BufRead};
+use std::io::{Write, BufReader, BufRead, Error, ErrorKind};
 
-fn main() -> std::io::Result<()> {
-    let mut address = String::from("127.0.0.1");
-    // println!("Enter the address of the chat server:");
-    // std::io::stdin().read_line(&mut address).unwrap();
-
+fn authenticate(address: &str) -> Result<(), &str> {
     let mut auth_token = String::new();
     println!("Enter the authentication code:");
     std::io::stdin().read_line(&mut auth_token).unwrap();
     let auth_token = auth_token.trim();
     let auth_message = format!("AUTHENTICATE\n{}\n", auth_token);
 
-    let mut stream = TcpStream::connect(format!("{0}:3030", address.trim()))?;
+    let mut stream = TcpStream::connect(format!("{0}:3030", address.trim())).unwrap();
 
-    stream.write(auth_message.as_bytes())?;
+    stream.write(auth_message.as_bytes()).unwrap();
 
     let reader = BufReader::new(&mut stream);
     let response = reader.lines().next().unwrap().unwrap();
-    println!("{}", response);
+
+    match &response[..] {
+        "OK" => Ok(()),
+        "BAD TOKEN" => Err("The authentication token is incorrect"),
+        _ => Err("Bad server response"),
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let mut address = String::from("127.0.0.1");
+    // println!("Enter the address of the chat server:");
+    // std::io::stdin().read_line(&mut address).unwrap();
+    
+    match authenticate(&address) {
+        Ok(_) => println!("Successfully authenticated"),
+        Err(e) => {
+            eprintln!("Authentication error: {}", e);
+            return Err(Error::from(ErrorKind::ConnectionRefused));
+        }
+    }
+
+    {
+        let mut stream = TcpStream::connect(format!("{0}:3030", address.trim()))?;
+
+        stream.write("MESSAGE\nsalut à tous les amis\n".as_bytes())?;
+    }
 
     Ok(())
 }
--- chat-server/src/main.rs
+++ chat-server/src/main.rs
@@ -4,6 +4,7 @@ use std::io::{Write, BufReader, BufRead};
 use chat_server::generate_auth_token;
 
 fn handle_client(mut stream: TcpStream, auth_token: &str) {
+    let sender_ip = stream.peer_addr().unwrap();
     let buf_reader = BufReader::new(&mut stream);
     let mut lines = buf_reader.lines();
     let request_line = lines.next().expect("HTTP request message is empty").expect("Error reading the request line");
@@ -13,10 +14,13 @@ fn handle_client(mut stream: TcpStream, auth_token: &str) {
     match &request_line[..] {
         "AUTHENTICATE" => {
             if auth_token == lines.next().unwrap().unwrap() {
-                stream.write(b"CONNECTED\n").unwrap();
+                stream.write(b"OK\n").unwrap();
             } else {
                 stream.write(b"BAD TOKEN\n").unwrap();
             }
+        }
+        "MESSAGE" => {
+            println!("{:?}: {}", sender_ip, lines.next().unwrap().unwrap());
         }
         _ => {
             stream.write(b"BAD REQUEST\n").unwrap();
